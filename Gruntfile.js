02e8dd6d0bf0b35a6d486191bcaec9496536ffea
Thabo <tha.bo.fletcher@gmail.com>
2015-09-28 17:43:55 -0700

    fix asset path

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,7 +412,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/landing',
           '<%= yeoman.dist %>/learn',
           '<%= yeoman.dist %>/badges',
-          '<%= yeoman.dist %>/swiftoberfest',
+          '<%= yeoman.dist %>/swifttoberfest',
           '<%= yeoman.dist %>/login',
           '<%= yeoman.dist %>/register'
         ],
